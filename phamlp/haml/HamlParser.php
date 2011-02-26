0b686b7ba6eb6e375aa26ffee2d4dac2c67bb555
Kyle Cunningham <kyle@codeincarnate.com>
2011-02-26 03:40:30 -0600

    Don't add in Haml helper file when parsing.  Would be better to include it by other means if it's needed.

--- phamlp/haml/HamlParser.php
+++ phamlp/haml/HamlParser.php
@@ -433,11 +433,6 @@ class HamlParser {
 	public function haml2PHP($sourceFile) {
 		$this->line = 0;
 		$this->filename = $sourceFile;
-		$helpers = "<?php\nrequire_once '".dirname(__FILE__).DIRECTORY_SEPARATOR."HamlHelpers.php';\n";
-		if (isset($this->helperFile)) {
-			$helpers .= "require_once '{$this->helperFile}';\n";
-		}
-		$helpers .= "?>";
 		return $helpers . $this->toTree(file_get_contents($sourceFile))->render();
 	}
 
